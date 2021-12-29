c04bd3ff8d7d16263962b04eb69e52c1997cf84a
MuhammadHammadRaza <79788124+MuhammadHammadRaza@users.noreply.github.com>
2021-12-29 21:59:12 +0500

    new

--- socialbump/src/components/Header/Profile/UserName.js
+++
@@ -1,9 +0,0 @@
-const UserName = (email) => {
-    let username = "";
-    const indexofAt = email.indexOf("@");
-    username = email.substring(0, indexofAt);
-
-    return username;
-}
-
-export default UserName;
